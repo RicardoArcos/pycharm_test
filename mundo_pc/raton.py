9a8b9f592d4bdf8c3f0ecd81ad2ecab712e8e3d6
Ricardo Arcos <ingricardoarcos@gmail.com>
2022-07-20 02:36:32 -0500

    probando el pycharm

---
+++ mundo_pc/raton.py
@@ -0,0 +1,12 @@
+from mundo_pc.dispositivo_entrada import DispositivoEntrada
+
+class Raton(DispositivoEntrada):
+    contador_ratones = 0
+
+    def __init__(self, marca, tipo_entrada):
+        Raton.contador_ratones += 1
+        self._id_raton = Raton.contador_ratones
+        super().__init__(marca, tipo_entrada)
+
+    def __str__(self):
+        return f'Id: {self._id_raton}, Marca: {self._marca}, Tipo_entrada: {self._tipo_entrada}'
